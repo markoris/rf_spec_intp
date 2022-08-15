39799e5405849c51429d64c499b1acdc451b91d1
Marko Ristic <mr3364@rit.edu>
2022-08-15 12:25:42 -0400

    run_rf_interpolator.py: added free time interpolation example

--- run_rf_interpolator.py
+++ run_rf_interpolator.py
@@ -1,6 +1,25 @@
+import os
 import numpy as np
 import rf_interpolator as rfi
 
-intp = rfi.intp_rf()
-intp.load_data('/home/marko.ristic/lanl/knsc1_active_learning/*spec*', './h5_data/TP_wind2_spectra.h5', t_max=21)
+os.environ['OMP_NUM_THREADS'] = "12"
+
+# fixed time, fixed angle interpolation
+##intp = rfi.intp_rf(seed=100)
+##intp.load_data('/home/marko.ristic/lanl/knsc1_active_learning/*spec*', './h5_data/TP_wind2_spectra.h5', t_max=21, short_wavs=False)
+##intp.create_test_set(size=5)
+##intp.preprocess()
+##intp.train(rf=True)
+##intp.evaluate()
+##intp.make_plots()
+
+# free time, fixed angle interpolation
+intp = rfi.intp_rf(seed=100)
+intp.load_data('/home/marko.ristic/lanl/knsc1_active_learning/*spec*', './h5_data/TP_wind2_spectra.h5', t_max=None, theta=0, short_wavs=False)
+intp.append_input_parameter(intp.times, 1)
 intp.create_test_set(size=5)
+intp.preprocess()
+intp.train(rf=True)
+intp.evaluate()
+intp.make_plots()
+
