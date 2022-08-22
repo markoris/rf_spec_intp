4ff1648173abf01b2c0d23ea424a90a4f5ac79c9
Marko Ristic <mristic@lanl.gov>
2022-08-22 13:08:42 -0600

    run_rf_interpolator.py: removing unnecessary argument for data load

--- run_rf_interpolator.py
+++ run_rf_interpolator.py
@@ -34,11 +34,11 @@ import rf_interpolator as rfi
 # free time, free angle interpolation # CURRENTLY TIMES OUT ON LANL HPC CLUSTER
 intp = rfi.intp_rf(seed=100, verbose=True, debugging=False)
 intp.load_data('/net/scratch4/mristic/knsc1_active_learning/*spec*', '/net/scratch4/mristic/h5_data/TP_wind2_spectra.h5', t_max=None, theta=None, short_wavs=False, downsample_theta=True)
-intp.append_input_parameter(intp.times, 1, var='time') # [N, time, wav, angle] -> [N, wav, angle]
-intp.append_input_parameter(intp.angles, 2, var='angle') # axis 1 = time, axis 2 = angle (originally angle = axis 3, but for fixed time we have -1 dimension)
+intp.append_input_parameter(intp.times, 1) # [N, time, wav, angle] -> [N, wav, angle]
+intp.append_input_parameter(intp.angles, 2) # axis 1 = time, axis 2 = angle (originally angle = axis 3, but for fixed time we have -1 dimension)
 intp.create_test_set(size=10)
 intp.preprocess()
-intp.train(rf=True)
+intp.train(rf=True, n_estimators=200)
 intp.evaluate()
 intp.make_plots()
 
