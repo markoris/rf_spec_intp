58b78b9e908b087a891b3ab66e463d71b13fac0f
Marko Ristic <mr3364@rit.edu>
2022-08-22 09:42:28 -0400

    renaming spectra interpolator example script

--- run_spectra_interpolator.py
+++ run_spectra_interpolator.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
-import rf_interpolator as rfi
+import spectra_interpolator as si
 
 # fixed time, fixed angle interpolation
-##intp = rfi.intp_rf(seed=100)
+##intp = si.intp_rf(seed=100)
 ##intp.load_data('/home/marko.ristic/lanl/knsc1_active_learning/*spec*', './h5_data/TP_wind2_spectra.h5', t_max=21, short_wavs=False)
 ##intp.create_test_set(size=5)
 ##intp.preprocess()
@@ -12,7 +12,7 @@ import rf_interpolator as rfi
 ##intp.make_plots()
 
 # free time, fixed angle interpolation
-##intp = rfi.intp_rf(seed=100)
+##intp = si.intp_rf(seed=100)
 ##intp.load_data('/net/scratch4/mristic/knsc1_active_learning/*spec*', '/net/scratch4/mristic/h5_data/TP_wind2_spectra.h5', t_max=None, theta=0, short_wavs=False)
 ##intp.append_input_parameter(intp.times, 1)
 ##intp.create_test_set(size=5)
@@ -22,7 +22,7 @@ import rf_interpolator as rfi
 ##intp.make_plots()
 
 # fixed time, free angle interpolation
-##intp = rfi.intp_rf(seed=100)
+##intp = si.intp_rf(seed=100)
 ##intp.load_data('/net/scratch4/mristic/knsc1_active_learning/*spec*', '/net/scratch4/mristic/h5_data/TP_wind2_spectra.h5', t_max=21, theta=None, short_wavs=False, downsample_theta=True)
 ##intp.append_input_parameter(intp.angles, 2) # axis 1 = time, axis 2 = angle (originally angle = axis 3, but for fixed time we have -1 dimension)
 ##intp.create_test_set(size=5)
@@ -32,7 +32,7 @@ import rf_interpolator as rfi
 ##intp.make_plots()
 
 # free time, free angle interpolation # CURRENTLY TIMES OUT ON LANL HPC CLUSTER
-intp = rfi.intp_rf(seed=100, verbose=True, debugging=False)
+intp = si.intp_rf(seed=100, verbose=True, debugging=False)
 intp.load_data('/net/scratch4/mristic/knsc1_active_learning/*spec*', '/net/scratch4/mristic/h5_data/TP_wind2_spectra.h5', t_max=None, theta=None, short_wavs=False, downsample_theta=True)
 intp.append_input_parameter(intp.times, 1, var='time') # [N, time, wav, angle] -> [N, wav, angle]
 intp.append_input_parameter(intp.angles, 2, var='angle') # axis 1 = time, axis 2 = angle (originally angle = axis 3, but for fixed time we have -1 dimension)
