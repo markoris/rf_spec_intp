739201fb5856706b7640c8b4d5fa6529427533ce
Marko Ristic <mristic@lanl.gov>
2023-04-04 11:29:04 -0600

    using optimized, trimmed data model

--- theta_prediction.py
+++ theta_prediction.py
@@ -14,18 +14,16 @@ at2017gfo_spectra = natsorted(at2017gfo_spectra)
 times_orig = [float(filename.split('/')[-1].split('+')[1].split('_')[0][:-1]) for filename in at2017gfo_spectra]
 times_orig = np.array(times_orig).reshape(-1, 1)
 
-intp = si.intp(seed=12)
-intp.load_data('/lustre/scratch4/turquoise/mristic/knsc1_active_learning/*spec*', '/lustre/scratch4/turquoise/mristic/h5_data/TP_wind2_spectra.h5', t_max=None, theta=float(args.theta), short_wavs=False)
+intp = si.intp(rf=True, seed=12)
+intp.load_data('/lustre/scratch4/turquoise/mristic/knsc1_active_learning/*spec*', '/lustre/scratch4/turquoise/mristic/h5_data/TP_wind2_spectra.h5', t_max=None, theta=float(args.theta), trim_dataset=True)
 intp.create_test_set(size=5)
 intp.append_input_parameter(intp.times, 1)
 intp.preprocess()
-intp.load('/lustre/scratch4/turquoise/mristic/rf_spec_intp_theta%sdeg.joblib' % args.theta)
+intp.load('/lustre/scratch4/turquoise/mristic/rf_spec_intp_optim_trimdata_theta%sdeg.joblib' % args.theta)
 
-param = intp.params_test[-2][:4]
-param = np.concatenate((param, intp.times[51].reshape(1))).reshape(1, -1) # 10-day time input
+param = intp.params_test[-1][:4]
+param = np.concatenate((param, intp.times[np.argmin(np.abs(intp.times-10.4))].reshape(1))).reshape(1, -1) # 10-day time input
 print(param)
-
-sys.exit()
 
 out = intp.evaluate(param, ret_out=True) # shape [n_draws*len(times), 1024]
 
--- time_prediction.py
+++ time_prediction.py
@@ -17,12 +17,12 @@ at2017gfo_spectra = natsorted(at2017gfo_spectra)
 times_orig = [float(filename.split('/')[-1].split('+')[1].split('_')[0][:-1]) for filename in at2017gfo_spectra]
 times_orig = np.array(times_orig).reshape(-1, 1)
 
-intp = si.intp(seed=12)
-intp.load_data('/lustre/scratch4/turquoise/mristic/knsc1_active_learning/*spec*', '/lustre/scratch4/turquoise/mristic/h5_data/TP_wind2_spectra.h5', t_max=float(time.replace("p", ".")), theta=None, short_wavs=False)
+intp = si.intp(rf=True, seed=12)
+intp.load_data('/lustre/scratch4/turquoise/mristic/knsc1_active_learning/*spec*', '/lustre/scratch4/turquoise/mristic/h5_data/TP_wind2_spectra.h5', t_max=float(time.replace("p", ".")), theta=None, trim_dataset=True)
 intp.create_test_set(size=5)
 intp.append_input_parameter(intp.angles, 2)
 intp.preprocess()
-intp.load('/lustre/scratch4/turquoise/mristic/rf_spec_intp_time%sdays.joblib' % time)
+intp.load('/lustre/scratch4/turquoise/mristic/rf_spec_intp_optim_trimdata_time%sdays.joblib' % time)
 
 param = intp.params_test[-1][:4]
 param = np.concatenate((param, intp.angles[0].reshape(1))).reshape(1, -1) # 0 degree angle input
