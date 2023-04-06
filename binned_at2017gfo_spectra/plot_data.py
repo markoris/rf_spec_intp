5808b977ff3c59e6729302831f73af0bdc310dea
Marko Ristic <mristic@lanl.gov>
2023-04-06 08:28:07 -0600

    seeing effects on linear scale

--- binned_at2017gfo_spectra/plot_data.py
+++ binned_at2017gfo_spectra/plot_data.py
@@ -14,6 +14,6 @@ for obs in range(len(data)):
     dat = dat[np.where(dat[:, 1] > 0)[0]]
     plt.plot(dat[:, 0]*1e-4, dat[:, 1]) # factor of 1e-4 on wavelengths to convert to microns
     plt.fill_between(dat[:, 0]*1e-4, dat[:, 1]-dat[:, 2], dat[:, 1]+dat[:, 2], alpha=0.3)
-    plt.xscale('log')
+    #plt.xscale('log')
     plt.savefig('t_%g.pdf' % times[obs])
     plt.close()
